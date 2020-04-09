e25b4e72beca78300aa22be4566298188d2ce708
Peti <meti651@gmail.com>
2020-04-09 10:37:06 +0200

    generator business logic

---
+++ Maze-API/Models/ModelMazeGenerator.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MazeGenerator;
+using MazeGenerator.Models;
+using MazePathFinder.Models;
+
+namespace Maze_API.Models
+{
+    public class ModelMazeGenerator
+    {
+        public Maze<Cell> NormalMaze { get; set; }
+        public Maze<PathFindingCell> PathFindingMaze { get; set; }
+        
+
+        public void GenerateNormalMaze(int row, int column)
+        {
+            MazeGenerator<Cell> generator = new MazeGenerator<Cell>(row, column);
+
+            generator.DeapthFirstMethod();
+
+            NormalMaze = generator.Maze;
+        }
+
+        public void GeneratePathFindingMaze(int row, int column)
+        {
+            MazeGenerator<PathFindingCell> generator = new MazeGenerator<PathFindingCell>(row, column);
+
+            generator.DeapthFirstMethod();
+
+            PathFindingMaze = generator.Maze;
+        }
+    }
+}
