d3386a9c8ee5581f6243ec8811f0f101e54faedf
Peti <meti651@gmail.com>
2020-04-09 10:28:00 +0200

    utility class

---
+++ MazeGenerator/Utility/Utils.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MazeGenerator.Utility
+{
+    public class Utils
+    {
+        static Random rand = new Random();
+
+        public static int GetRandomNumber(int min, int max)
+        {
+            return rand.Next(min, max);
+        }
+    }
+}
