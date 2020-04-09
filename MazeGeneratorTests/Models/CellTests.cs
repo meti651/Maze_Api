2e5c70a191da9d0f28df2b13a9de79a713cbe8d5
Peti <meti651@gmail.com>
2020-04-09 10:30:59 +0200

    maze model tests

---
+++ MazeGeneratorTests/Models/CellTests.cs
@@ -0,0 +1,83 @@
+﻿using MazeGenerator.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MazeGenerator.Models.Tests
+{
+    [TestClass()]
+    public class CellTests
+    {
+        [TestMethod()]
+        public void CellIsNotNull()
+        {
+            Cell newCell = new Cell(0, 0);
+            Assert.IsNotNull(newCell);
+        }
+
+        [TestMethod()]
+        public void MakeLinkWallsDesapear()
+        {
+            Cell firstCell = new Cell(0, 0);
+            Cell secondCell = new Cell(0, 1);
+
+            firstCell.MakeLink(secondCell);
+            Assert.IsFalse(firstCell.EastWall);
+        }
+
+        [TestMethod()]
+        public void MakeLinkCellAdded()
+        {
+            Cell firstCell = new Cell(0, 0);
+            Cell linkCell = new Cell(0, 1);
+
+            firstCell.MakeLink(linkCell);
+
+            Assert.IsTrue(firstCell.Links.Count > 0);
+        }
+
+        [TestMethod()]
+        public void AddSiblingAddCell()
+        {
+            Cell firstCell = new Cell(0, 0);
+            Cell secondCell = new Cell(0, 1);
+
+            firstCell.AddSibling(secondCell);
+
+            Assert.IsTrue(firstCell.Siblings.Contains(secondCell));
+        }
+
+        [TestMethod()]
+        public void GetRandomUnvisitedSiblingReturnsNull()
+        {
+            HashSet<Cell> visitedCells = new HashSet<Cell>();
+
+            Cell newCell = new Cell(0, 0);
+            Cell visitedSibling = new Cell(0, 1);
+
+            newCell.AddSibling(visitedSibling);
+            visitedCells.Add(visitedSibling);
+
+            Cell unvisitedCell = newCell.GetRandomUnvisitedSibling<Cell>(visitedCells);
+
+            Assert.IsNull(unvisitedCell);
+        }
+
+        [TestMethod()]
+        public void GetRandomUnvisitedSiblingReturnsCell()
+        {
+            HashSet<Cell> visitedCells = new HashSet<Cell>();
+
+            Cell newCell = new Cell(0, 0);
+            Cell unvisitedSibling = new Cell(0, 1);
+
+            newCell.AddSibling(unvisitedSibling);
+
+            Cell unvisitedCell = newCell.GetRandomUnvisitedSibling<Cell>(visitedCells);
+
+            Assert.AreEqual(unvisitedSibling, unvisitedCell);
+        }
+    }
+}
---
+++ MazeGeneratorTests/Models/MazeTests.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MazeGenerator.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using MazeGenerator.Utility;
+
+namespace MazeGenerator.Models.Tests
+{
+    [TestClass()]
+    public class MazeTests
+    {
+        [TestMethod()]
+        public void MazeHaveItsMatrix()
+        {
+            Maze<Cell> maze = new Maze<Cell>(3, 3);
+
+            Assert.IsNotNull(maze.Cells);
+        }
+
+        [TestMethod()]
+        public void IntializeCellsReturnsMatrixFullOfCells()
+        {
+            Maze<Cell> maze = new Maze<Cell>(3, 3);
+
+            int randRow = Utils.GetRandomNumber(0, 3);
+            int randColumn = Utils.GetRandomNumber(0, 3);
+
+            Assert.IsNotNull(maze.Cells[randRow, randColumn]);
+        }
+
+        [TestMethod()]
+        public void InitializeCellsCellsGotPosition()
+        {
+            Maze<Cell> maze = new Maze<Cell>(3, 3);
+
+            Cell firstCell = maze.Cells[0, 0];
+
+            Assert.AreEqual(0, firstCell.X_Position);
+        }
+
+
+        [TestMethod()]
+        public void GetRandomCellReturnsCell()
+        {
+            Maze<Cell> maze = new Maze<Cell>(3, 3);
+
+            Cell randomCell = maze.GetRandomCell();
+
+            Assert.IsNotNull(randomCell);
+        }
+
+        [TestMethod()]
+        public void InitializeSiblingsSiblingsIsNotNull()
+        {
+            Maze<Cell> maze = new Maze<Cell>(3, 3);
+
+            maze.InitializeSiblings();
+
+            Cell firstCell = maze.Cells[0, 0];
+
+            Assert.IsTrue(firstCell.Siblings.Count > 0);
+        }
+
+        [TestMethod()]
+        public void InitializeSiblingsAddCorrectCell()
+        {
+            Maze<Cell> maze = new Maze<Cell>(3, 3);
+
+            maze.InitializeSiblings();
+
+            Cell firstCell = maze.Cells[0, 0];
+            Cell siblingCell = maze.Cells[0, 1];
+
+            Assert.IsTrue(firstCell.Siblings.Contains(siblingCell));
+        }
+
+        [TestMethod()]
+        public void InitializeSiblingsAddCorrectAmountOfCells()
+        {
+            Maze<Cell> maze = new Maze<Cell>(3, 3);
+
+            Cell firstCell = maze.Cells[0, 0];
+
+            Assert.IsTrue(firstCell.Siblings.Count == 2);
+        }
+    }
+}
